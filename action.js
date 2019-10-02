998ac8ac65e4f670232c481ab1c3ae0299934538
Diablo9946 <diabloch9946@gmail.com>
2019-10-02 17:55:30 +0200

    API working

--- action.js
+++ action.js
@@ -1,29 +1,58 @@
-url = "https://api.themoviedb.org/3/movie/550?api_key=13e4dd0bfdea1b550587c2e9a8eb9391";
-var stamp = document.getElementById("entry-template").innerHTML;
+url = "https://api.themoviedb.org/3/search/movie";
 
 
-$(document).ready(function( ){
+$(document).ready(function(){
+
+
+$("#searchButt").click(function(){
+  console.log("cliccato il bottone");
+  var query = $("#searchBar").val();
+  console.log(query);
 
 
 
-$.ajax {(
-  url : url,
-  method : "GET";
-  success : function (data) {
+  $.ajax({
+    url : url,
+    method : "GET",
+    data : {
+      api_key : "13e4dd0bfdea1b550587c2e9a8eb9391",
+      language : "it-IT",
+      query : query
+    },
+    success : function (data) {
+      console.log("Sono nella chiamata API");
+      console.log(data);
+      var source   = $("#template").html();
+      var template = Handlebars.compile(source);
+      var obj = $(".obj");
+      var ris = data.results;
 
-    var source   = $("#template").html();
-    var template = Handlebars.compile(source);
-    var context = {
-      title: "",
-      orgtitle: "",
-      lang: "",
-      voto: ""
-    };
-    var html    = template(context);
-  },
-  error : function (){
+      for (i=0; i< ris.length; i++) {
 
-  }
-)}
 
-)};
+
+        var context = {
+          title: ris.title,
+          orgtitle: ris.original_title,
+          lang: ris.original_language,
+          voto: ris.vote_average,
+          img: "<img src='ris.poster_path' >"
+        };
+
+        obj.append(ris[i].title, "<br>");
+      };
+        var html = template(context);
+    },
+    error : function (){
+
+    }
+  });
+
+
+});
+
+
+
+console.log("Sono nel documento");
+
+});
