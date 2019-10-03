31be4d925790666d245aa1547d15165812b6af9a
Diablo9946 <diabloch9946@gmail.com>
2019-10-03 06:41:20 +0200

    Now I have to make the 2 functions as one and to make it dynamic.

--- action.js
+++ action.js
@@ -1,16 +1,16 @@
-url = "https://api.themoviedb.org/3/search/movie";
-
 
 $(document).ready(function(){
 
+var obj = $(".obj");
 
 $("#searchButt").click(function(){
   console.log("cliccato il bottone");
   var query = $("#searchBar").val();
   console.log(query);
 
+// MOVIES
   $.ajax({
-    url : url,
+    url : "https://api.themoviedb.org/3/search/movie",
     method : "GET",
     data : {
       api_key : "13e4dd0bfdea1b550587c2e9a8eb9391",
@@ -21,40 +21,133 @@ $("#searchButt").click(function(){
       console.log("Sono nella chiamata API");
       console.log(data);
 
-      var obj = $(".obj");
+
       var ris = data.results;
       printMovies(ris);
-        // for (i=0; i< ris.length; i++) {
-        //   var context = {
-        //     title: ris[i].title,
-        //     orgtitle: ris[i].original_title,
-        //     lang: ris[i].original_language,
-        //     voto: ris[i].vote_average,
-        //     img: "<img src='ris[i].poster_path' >"
-        //   };
-        //
-        //   obj.append(ris[i].title + "<br>" + ris[i].original_title, "<br><br>");
-        //   obj.append(context);
-        // };
-        //   var html = template(context);
 
-      // };
     },
     error : function (){
 
     }
   });
+  // TV SERIES
+  $.ajax({
+    url : "https://api.themoviedb.org/3/search/tv",
+    method : "GET",
+    data : {
+      api_key : "13e4dd0bfdea1b550587c2e9a8eb9391",
+      language : "it-IT",
+      query : query
+    },
+    success : function (data) {
+      console.log("Sono nella chiamata API");
+      console.log(data);
+
+
+      var ris = data.results;
+      printSeries(ris);
+
+    },
+    error : function (){
+
+    }
+  });
+
 });
 
+
+
+
+
+
 function printMovies(ris){
-  for (i=0; i< ris.length; i++) {
+  for (var i=0; i< ris.length; i++) {
     var movie = ris[i];
     var source   = $("#template").html();
     var template = Handlebars.compile(source);
+
+    var context = {
+      type: "Film",
+      title: movie.title,
+      orgtitle: movie.original_title,
+      lang: getFlag(movie.original_language),
+      // lang: movie.original_language,
+      voto: getStarFromRates(movie.vote_average),
+      // voto: movie.vote_average,
+      // img: "<img src='" + movie.poster_path + "' >"
+    };
+
+    var html = template(context);
+
+    obj.append(html);
   }
 };
 
 
+function printSeries(ris){
+  for (var i=0; i< ris.length; i++) {
+    var movie = ris[i];
+    var source   = $("#template").html();
+    var template = Handlebars.compile(source);
+
+    var context = {
+      type: "Serie Tv",
+      title: movie.name,
+      orgtitle: movie.original_name,
+      lang: getFlag(movie.original_language),
+      // lang: movie.original_language,
+      voto: getStarFromRates(movie.vote_average),
+      // voto: movie.vote_average,
+      // img: "<img src='" + movie.poster_path + "' >"
+    };
+
+    var html = template(context);
+
+    obj.append(html);
+  }
+
+};
+
 console.log("Sono nel documento");
+
+
+function getStarFromRates (rate){
+  var roundedRate = Math.floor(rate/2);
+  var graphStar = "";
+  for (var i=0; i<5; i++){
+    if (i<roundedRate){
+      graphStar += "<i class='fas fa-star'></i>";
+    } else {
+      graphStar += "<i class='far fa-star'></i>";
+    };
+    // <i class="fas fa star"></i>
+    // <i class="far fa star"></i>
+  }
+
+  return graphStar;
+};
+
+
+function getFlag(lang){
+  // if (lang === "en"){
+  //   lang = '<img src="img/en.png">'
+  // } if (lang === "it"){
+  //   lang = '<img src="img/it.png">'
+  // }
+  var avaiableFlag = [
+    "it",
+    "en"
+  ];
+
+  var flag = "";
+  if (avaiableFlag.includes(lang)){
+
+    flag = "<img src='img/" + lang + ".png'>";
+  } else {
+
+    flag = lang;
+  };
+  return flag;
+};
 
 });
